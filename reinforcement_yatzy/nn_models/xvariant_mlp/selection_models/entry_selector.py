41769a050d6277c892e50f0e923ed710088f2d9e
aniwlem <melwinvandersson@gmail.com>
2024-07-08 15:41:28 +0200

    added scoreboard entry selection model

---
+++ reinforcement_yatzy/nn_models/xvariant_mlp/selection_models/entry_selector.py
@@ -0,0 +1,83 @@
+import torch
+from torch import nn
+
+from reinforcement_yatzy.nn_models.autoencoders.scoreboard_autoencoder import ScoreboardEncoder
+from reinforcement_yatzy.nn_models.xvariant_mlp.base_models.invariant_mlp import InvariantMLP, InvariantPoolingParams
+from reinforcement_yatzy.nn_models.xvariant_mlp.pool_type_enum import PoolType
+
+
+class EntrySelector(nn.Module):
+    def __init__(
+        self,
+        n_dice: int,
+        n_entries: int,
+        dice_pre_mlp_channels: list[int],
+        dice_pre_mlp_pool_type: PoolType,
+        dice_invarintifier_pool_types: InvariantPoolingParams,
+        scoreboard_encoder: ScoreboardEncoder,
+        mlp_dims: list[int],
+    ) -> None:
+        super().__init__()
+
+        self.dice_invariantifier = InvariantMLP(
+            n_elems=n_dice,
+            embed_dim=1,  # just the dice values
+            mlp_channels=[1, *dice_pre_mlp_channels],
+            mlp_pool_type=dice_pre_mlp_pool_type,
+            invarintifier_pool_types=dice_invarintifier_pool_types,
+        )
+        self.scoreboard_encoder = scoreboard_encoder
+
+        mlp_layers = [
+            nn.Linear(
+                self.scoreboard_encoder.latent_dim + dice_pre_mlp_channels[-1],
+                mlp_dims[0],
+            ),
+            nn.ReLU(),
+        ]
+
+        for i in range(len(mlp_dims) - 1):
+            mlp_layers.extend([
+                nn.Linear(
+                    mlp_dims[i],
+                    mlp_dims[i + 1],
+                ),
+                nn.ReLU(),
+            ])
+
+        mlp_layers.extend([
+            nn.Linear(
+                mlp_dims[-1],
+                n_entries,
+            ),
+            nn.ReLU(),
+        ])
+
+        self.mlp_layers = nn.ModuleList(mlp_layers)
+
+    def forward(self, dices: torch.Tensor, scoreboards: torch.Tensor) -> torch.Tensor:
+        '''
+        inputs:
+        dice: shape[batch_size, n_dice]
+        scoreboards: shape[batch_size, n_entries]
+
+        output: shape[batch_size, n_entries]
+        '''
+
+        # add channel and embed dims
+        batch_size, n_dice = dices.shape
+        dice_batch = dices.reshape([batch_size, 1, n_dice, 1])
+        invariant_dice = self.dice_invariantifier(dice_batch)
+
+        # the encoder will add channel and embed dims itself
+        scoreboard_embeddings = self.scoreboard_encoder(scoreboards)
+        print(scoreboard_embeddings.shape)
+        batch = torch.concat([
+            invariant_dice,
+            scoreboard_embeddings,
+        ], dim=1)
+
+        for layer in self.mlp_layers:
+            batch = layer(batch)
+
+        return batch
---
+++ tests/test_xvariant_mlp/test_entry_selector.py
@@ -0,0 +1,64 @@
+import pytest
+from itertools import permutations
+
+import numpy as np
+import torch
+
+from reinforcement_yatzy.nn_models.xvariant_mlp.base_models.invariant_mlp import InvariantPoolingParams
+from reinforcement_yatzy.nn_models.xvariant_mlp.selection_models.entry_selector import EntrySelector
+from reinforcement_yatzy.nn_models.autoencoders.scoreboard_autoencoder import ScoreboardEncoder
+
+from reinforcement_yatzy.nn_models.xvariant_mlp.pool_type_enum import PoolType
+
+
+class TestEntrySelector:
+    n_dice = 3
+    n_entries = 13
+    dice_embed_dim = 2
+    scoreboard_embed_dim = 5
+
+    @pytest.fixture
+    def encoder(self):
+        return ScoreboardEncoder(
+            input_dim=self.n_entries,
+            mlp_dims=[3, 5, 7],
+            latent_dim=self.scoreboard_embed_dim
+        )
+
+    @pytest.fixture
+    def entry_selector(self, encoder: ScoreboardEncoder):
+        return EntrySelector(
+            n_dice=self.n_dice,
+            n_entries=self.n_entries,
+            dice_pre_mlp_channels=[6, 2, 4],
+            dice_pre_mlp_pool_type=PoolType.AVG,
+            dice_invarintifier_pool_types=InvariantPoolingParams(
+                embed_pooling=PoolType.AVG,
+                elem_pooling=PoolType.AVG,
+            ),
+            scoreboard_encoder=encoder,
+            mlp_dims=[16, 32, 16, 8, self.n_entries],
+        )
+
+    @pytest.mark.parametrize('batch_size', range(1, 10))
+    def test_forward(self, entry_selector: EntrySelector, batch_size: int):
+        dices = torch.rand([batch_size, self.n_dice])
+        entries = torch.rand([batch_size, self.n_entries])
+
+        assert list(entry_selector(dices, entries).shape) == [
+            batch_size,
+            self.n_entries
+        ]
+
+    @pytest.mark.parametrize('batch_size', range(1, 10))
+    def test_invariance(self, entry_selector: EntrySelector, batch_size: int):
+        dices = torch.rand([batch_size, self.n_dice])
+        entries = torch.rand([batch_size, self.n_entries])
+
+        results = entry_selector(dices, entries)
+        perms = list(permutations(range(self.n_dice)))
+
+        for perm in perms:
+            assert np.all(
+                (entry_selector(dices[:, perm], entries) == results).numpy
+            )
