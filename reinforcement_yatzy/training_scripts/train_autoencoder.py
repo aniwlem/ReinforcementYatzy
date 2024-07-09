f1324be7fd4acdfcdfd4b5ffe1ff974a4402aaee
aniwlem <melwinvandersson@gmail.com>
2024-07-09 12:12:45 +0200

    improved script for training autoencoder

--- reinforcement_yatzy/training_scripts/train_autoencoder.py
+++ reinforcement_yatzy/training_scripts/train_autoencoder.py
@@ -1,11 +1,14 @@
 import argparse
+from datetime import datetime
 from pathlib import Path
+import os
 
+import numpy as np
 import pandas as pd
 import torch
 from torch import nn
 from torch import optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 
 from reinforcement_yatzy.scoreboard_dataset.scoreboard_dataset import ScoreboardDataset
@@ -37,47 +40,105 @@ def setup_autoencoder(encoder_dims: list[int], latent_dim: int) -> ScoreboardAut
 
 def train_autoencoder(
     autoencoder: ScoreboardAutoencoder,
-    data_loader: DataLoader,
-    epochs: int,
+    train_loader: DataLoader,
+    val_loader: DataLoader,
+    n_epochs: int,
+    loss_path: Path,
+    n_rising_until_break: int,
 ):
+    '''Train the autoencoder with holdout validation'''
     criterion = nn.MSELoss()
     optimizer = optim.Adam(autoencoder.parameters(), lr=1e-3)
 
-    for epoch in range(epochs):
-        with tqdm(data_loader, unit='batch') as tepoch:
-            for batch in tepoch:
-                tepoch.set_description(f'Epoch {epoch}')
+    n_epochs_val_rising = 0
+    train_losses = np.zeros([n_epochs])
+    val_losses = np.zeros([n_epochs])
+
+    for i_epoch in range(n_epochs):
+        # Training
+        with tqdm(train_loader, unit='batch') as tepoch:
+            sum_loss = 0
+            for i_batch, batch in enumerate(tepoch):
+                tepoch.set_description(f'Epoch {i_epoch}')
 
                 optimizer.zero_grad()
                 outputs = autoencoder(batch)
                 loss = criterion(outputs, batch)
                 loss.backward()
                 optimizer.step()
+                sum_loss += loss.item()
 
-                tepoch.set_postfix(loss=loss.item())
+                tepoch.set_postfix(loss=sum_loss / (i_batch + 1))
+
+        avg_train_loss = sum_loss / len(train_loader)
+        train_losses[i_epoch] = avg_train_loss
+
+        # Validation
+        with torch.no_grad():
+            sum_val_loss = 0
+            for batch in val_loader:
+                outputs = autoencoder(batch)
+                val_loss = criterion(outputs, batch)
+                sum_val_loss += val_loss
+
+        avg_val_loss = sum_val_loss / len(val_loader)
+        val_losses[i_epoch] = avg_val_loss
+
+        tqdm.write(
+            f'\nEpoch {i_epoch} - Training Loss: {avg_train_loss:.2e} - Validation Loss: {avg_val_loss:.2e}\n')
+
+        # Holdout validation
+        if i_epoch > 0 and val_losses[i_epoch] > val_losses[i_epoch - 1]:
+            n_epochs_val_rising += 1
+
+        if n_epochs_val_rising == n_rising_until_break:
+            break
+
+    np.savetxt(
+        loss_path,
+        np.column_stack([train_losses, val_losses]),
+        delimiter=',',
+        header='train_loss,val_loss',
+    )
+
     return autoencoder
 
 
-def main(save_path: Path, dataset_path: Path, epochs: int):
-    encoder_dims = [32, 16, 8]
-    latent_dim = 4
+def main(save_path: Path, dataset_path: Path, loss_log_dir_path: Path, epochs: int):
+    assert os.path.exists(loss_log_dir_path), 'Invalid directory for loss log'
+
+    encoder_dims = [32, 32, 16, 16, 8]
+    latent_dim = 6
     autoencoder = setup_autoencoder(encoder_dims, latent_dim)
 
     dataset_df = pd.read_csv(dataset_path)
     dataset = ScoreboardDataset(dataset_df)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    train_set, val_set = random_split(dataset, [.95, 0.05])
+    train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=32, shuffle=True)
 
-    train_autoencoder(autoencoder, dataloader, epochs)
+    now = datetime.now()
+    formatted_time = now.strftime("%m-%d_%H-%M")
+    loss_file_path = Path(''.join([
+        'auto_encoder_',
+        '[',
+        *[str(dim) + '_' for dim in encoder_dims],
+        ']_',
+        f'{latent_dim}_',
+        formatted_time,
+        '.csv'
+    ]))
+    loss_path = Path(os.path.join(loss_log_dir_path, loss_file_path))
+    train_autoencoder(
+        autoencoder,
+        train_loader,
+        val_loader,
+        epochs,
+        loss_path=loss_path,
+        n_rising_until_break=5
+    )
     torch.save(autoencoder.encoder.state_dict(), save_path)
     print(f'Saved weights to {save_path}')
-
-    player = TrainingYatzyPlayer()
-    player.throw_dice(range(5))
-    player.check_points_of_dice()
-    in_board = torch.Tensor(list(player.curr_possible_scores.values()))
-    out_board = autoencoder(in_board)
-    print(
-        f'input: {in_board.detach().numpy()}\noutput: {out_board.detach().numpy()}')
 
 
 if __name__ == '__main__':
@@ -87,6 +148,8 @@ if __name__ == '__main__':
                         help="Path to save the model weights.")
     parser.add_argument("dataset_path", type=Path,
                         help="Path to the scoreboard dataset")
+    parser.add_argument("loss_log_dir_path", type=Path,
+                        help="Path to the directory with loss logs")
     parser.add_argument("epochs", type=int,
                         help="Number of epochs to train")
     args = parser.parse_args()
